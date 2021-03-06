deedf305d98cb227bb6939d22d5ca4320c2eeb35
David Tito <jorge.tito@valtech.com>
2021-03-06 19:47:19 -0300

    added service film

--- src/services/SwapiServices.js
+++ src/services/SwapiServices.js
@@ -3,3 +3,5 @@ import api from '../config/api';
 export const getShips = async (query = '') => api.get(`starships/${query}`);
 
 export const getPilots = async (query = '') => api.get(`people/${query}`);
+
+export const getFilms = async (query = '') => api.get(`films/${query}`);
